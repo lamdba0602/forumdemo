c8be9a59d7988e0e567b81ec1bbaca5927510823
dangming <dangming@unitedstack.com>
2016-08-10 11:56:19 +0800

    from and article detail

--- forumdemo/article/urls.py
+++ forumdemo/article/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import article_list
 from .views import article_create
+from .views import article_detail
+from .views import article_list
 
 urlpatterns = [
     url(r'^list/(?P<block_id>\d+)', article_list),
     url(r'^create/(?P<block_id>\d+)', article_create),
+    url(r'^detail/(?P<article_id>\d+)', article_detail),
 ]
--- forumdemo/article/views.py
+++ forumdemo/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
 
+from article.forms import ArticleForm
 from article.models import Article
 from block.models import Block
 
@@ -18,16 +19,19 @@ def article_create(request, block_id):
     if request.method == "GET":
         return render(request, "article_create.html", {"b": block})
     else:
-        title = request.POST["title"].strip()
-        content = request.POST["content"].strip()
-        if not title or not content:
-            return render(request, "article_create.html",
-                    {"b": block, "error": "标题和内容都不能为空",
-                    "title": title, "content": content})
-        if len(title) > 100 or len(content) > 10000:
-            return render(request, "article_create.html",
-                    {"b": block, "error": "标题或内容太长了",
-                    "title": title, "content": content})
-        article = Article(block=block, title=title, content=content, status=0)
-        article.save()
-        return redirect("/article/list/%s" % block_id)
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.block = block
+            article.status = 0
+            article.save()
+            return redirect("/article/list/%s" % block_id)
+        else:
+            return render(request, "article_create.html", {"b": block, "form": form})
+
+
+def article_detail(request, article_id):
+    article_id = int(article_id)
+    article = Article.objects.get(id=article_id)
+
+    return render(request, "article_detail.html", {"article": article})
