4f51f4c811ed2a3442deeea55e5eb0b5f1d433ae
dangming <dangming@unitedstack.com>
2016-10-01 18:15:20 +0800

    fix message AnonymousUser

--- forumdemo/views.py
+++ forumdemo/views.py
@@ -7,6 +7,8 @@ from message.models import Message
 def index(request):
     block_infos = Block.objects.filter(status=0).order_by("-id")
     owner = request.user
-    messages = Message.objects.filter(owner=owner, status=0)
-    msg_cnt = messages.count()
+    if request.user.is_authenticated():
+        msg_cnt = Message.objects.filter(owner=owner, status=0).count()
+    else:
+        msg_cnt = 0
     return render(request, "index.html", {"blocks": block_infos, "msg_cnt": msg_cnt})
