12325d8e46bdd10955e50dda2f4fabd7a3c180d2
dangming <dangming@unitedstack.com>
2016-10-09 19:16:18 +0800

    mysql instead of sqllite

--- forumdemo/forumdemo/settings.py
+++ forumdemo/forumdemo/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'forumdemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'forumdemo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -109,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "dist_static")
 
 from .secret import *
 LOGIN_REDIRECT_URL = "/"
